fab7268e3884ea6ea832d298dac3c123908bd94e
David Thai <david.thai@dafuloth.me>
2018-08-27 20:39:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,9 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, newElement) {
   return [newElement, ...array]
 }
+
+
+function destructivelyAddElementToBeginningOfArray(array, newElement) {
+  array.unshift(newElement)
+  return array
+}
