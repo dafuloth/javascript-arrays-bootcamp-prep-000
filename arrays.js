76e96177a67156ca628cf6168c08e0300914e2c7
David Thai <david.thai@dafuloth.me>
2018-08-27 20:46:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,7 +24,7 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array = array[1:]
+
 }
 
 // function destructivelyRemoveElementFromEndOfArray(array) {
