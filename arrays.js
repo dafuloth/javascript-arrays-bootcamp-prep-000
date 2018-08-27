0f4138a92995babdec83bebedc4be5f445f66c91
David Thai <david.thai@dafuloth.me>
2018-08-27 20:33:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray(array, newElement) {
-  return array(array.unshift(newElement))
+function addElementToBeginningOfArray(array[], newElement) {
+  return array.unshift(newElement)
 }
